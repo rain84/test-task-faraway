8029bb9721d657b5a6f8e67d5ff86042c9c89657
rain84 <rainy_sky@mail.ru>
2022-03-12 17:11:08 +0300

    ♻️ refact: some changes

--- src/layout/info.component.tsx
+++ src/layout/info.component.tsx
@@ -2,7 +2,7 @@ import { Table } from 'ui'
 import fakeData from 'fake_data.json'
 
 const data = fakeData.data[0] as unknown as Record<string, string>
-const fields = [
+const parameters = [
   'height',
   'mass',
   'hair_color',
@@ -11,14 +11,14 @@ const fields = [
   'birth_year',
   'gender',
 ]
+const columns = ['parameter', 'description']
+const rows = parameters.map((parameter) => ({
+  parameter,
+  description: data[parameter],
+}))
+const getKey = ({ parameter }: Record<string, string>) => parameter
 
 export const Info = () => {
-  const rows = fields.map((field) => ({
-    parameter: field,
-    description: data[field],
-  }))
-
-  const getKey = ({ info }: Record<string, string>) => info
   return (
     <section>
       <h1 className="text-4xl font-extrabold tracking-tight text-center text-gray-900 sm:text-5xl md:text-6xl">
@@ -28,7 +28,7 @@ export const Info = () => {
       <main className="flex flex-row justify-center">
         <Table
           className="w-1/2"
-          columns={['parameter', 'description']}
+          columns={columns}
           rows={rows}
           getKey={getKey}
         />
